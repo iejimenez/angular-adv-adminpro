5f3eef61d2881a0dc959df97cf1a21f1e5ef516f
Ivan Jimenez <iejimenez01@gmail.com>
2022-06-03 10:13:21 -0500

    curso angular fin seccion 3 - diseño

---
+++ src/app/pages/garfica1/garfica1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garfica1',
+  templateUrl: './garfica1.component.html',
+  styles: [
+  ]
+})
+export class Garfica1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
