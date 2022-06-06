fadccdd0e97f3cd79ac558bf46aa3a8757c08917
Ivan Jimenez <iejimenez01@gmail.com>
2022-06-06 08:20:05 -0500

    Fin sección 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,24 @@
 import { Component, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./auth/login/login.component";
-import { RegisterComponent } from "./auth/register/register.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { Garfica1Component } from "./pages/garfica1/garfica1.component";
-import { NopagefoundComponent } from "./pages/nopagefound/nopagefound.component";
-import { PagesComponent } from "./pages/pages.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
+import { AuthRoutingModule } from "./auth/auth.routing";
+import { PagesRoutingModule } from "./pages/pages.routing";
+
+import { NopagefoundComponent } from "./nopagefound/nopagefound.component";
 
 const routes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: Garfica1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-
-    { path: '**', component: NopagefoundComponent },
-
+    //path: '/dashboard' PagesRouting
+    //path: '/login' or '/register' AuthRouting
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '**', component: NopagefoundComponent }
 ]
 
 @NgModule({
     declarations:[],
-    imports: [ RouterModule.forRoot(routes) ],
+    imports: [ 
+        RouterModule.forRoot(routes),
+        PagesRoutingModule,
+        AuthRoutingModule
+    ],
     exports: [RouterModule]
 })
 
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ]
+})
+export class AuthModule { }
